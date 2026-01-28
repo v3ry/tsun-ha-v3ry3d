5c4bdfa3668d68a0a87ad5bf8c2fe8c5b18fe778
v3ry <v3ryfr@gmail.com>
2026-01-28 18:24:35 +0100

    Refactor config flow to use selector for username and password inputs

--- custom_components/tsun_monitoring/config_flow.py
+++ custom_components/tsun_monitoring/config_flow.py
@@ -9,18 +9,11 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.selector import selector
 
-from .api import TsunMonitoringAPI
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_USERNAME): str,
-        vol.Required(CONF_PASSWORD): str,
-    }
-)
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -32,35 +25,31 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA,
-                description_placeholders={},
-            )
-
         errors = {}
 
-        try:
-            api = TsunMonitoringAPI(
-                username=user_input[CONF_USERNAME],
-                password=user_input[CONF_PASSWORD],
-            )
-            await self.hass.async_add_executor_job(api.authenticate)
-        except Exception as err:  # pylint: disable=broad-except
-            _LOGGER.error("Unexpected exception: %s", err)
-            errors["base"] = "cannot_connect"
-        else:
+        if user_input is not None:
+            # Check if already configured
             await self.async_set_unique_id(user_input[CONF_USERNAME])
             self._abort_if_unique_id_configured()
+
             return self.async_create_entry(
                 title=f"TSUN ({user_input[CONF_USERNAME]})",
                 data=user_input,
             )
 
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): selector(
+                    {"text": {"type": "email"}}
+                ),
+                vol.Required(CONF_PASSWORD): selector(
+                    {"text": {"type": "password"}}
+                ),
+            }
+        )
+
         return self.async_show_form(
             step_id="user",
-            data_schema=STEP_USER_DATA_SCHEMA,
+            data_schema=data_schema,
             errors=errors,
-            description_placeholders={},
         )
